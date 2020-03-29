944141be1f901a8f6b0716419753c456870c1ebc
Jose Maria Garcia Garcia <josemariagarcia95@gmail.com>
2020-03-29 18:56:53 +0200

    Barcode and toast working for books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Toast } from '@ionic-native/toast/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { AppComponent } from './app.component';
     StatusBar,
     SplashScreen,
     BarcodeScanner,
+    Toast,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
-import { IonApp } from '@ionic/angular';
+import { Toast } from '@ionic-native/toast/ngx';
 
 @Component( {
   selector: 'app-tab3',
@@ -8,54 +8,46 @@ import { IonApp } from '@ionic/angular';
   styleUrls: [ 'tab3.page.scss' ]
 } )
 export class Tab3Page {
-  constructor( private barcodeScanner: BarcodeScanner ) {}
+  constructor( private barcodeScanner: BarcodeScanner, private toast: Toast ) {}
 
   click( ) {
     const optionsBarcodeScanner: BarcodeScannerOptions = {
-      preferFrontCamera: true,
-      showFlipCameraButton: true,
       showTorchButton: true,
       torchOn: false,
-      prompt: 'Place a barcode inside the scan area',
+      prompt: 'Coloca código de barras en el recuadro',
       resultDisplayDuration: 500,
-      formats: 'QR_CODE,PDF_417 ',
-      orientation: 'landscape',
+      formats: 'EAN_13, EAN_8',
     };
     console.log( 'Llego a cambiar los estilos' );
     // First try -add 'transparent none !important' backgrounds styles on ion-content, app-tabs, 
     // app-root, ion-app, ion-content and body - Doesn't work, neither on emulator or device
-    const ionContent = document.querySelector('ion-content') as HTMLElement;
-    ionContent.classList.add('has-camera');
-    const appTabs = document.querySelector( 'app-tabs' ) as HTMLElement;
-    appTabs.classList.add( 'has-camera' );
-    const appRoot = document.querySelector( 'app-root' ) as HTMLElement;
-    appRoot.classList.add( 'has-camera' );
-    const ionApp = document.querySelector( 'ion-app' ) as HTMLElement;
-    ionApp.classList.add( 'has-camera' );
-    const body = document.querySelector( 'body' ) as HTMLElement;
-    body.classList.add( 'has-camera' );
-    /*
-    */
-    //ionApp.style['z-index'] = -1000;
-    //const appRoot = document.querySelector('app-root') as HTMLElement;
-    //appRoot.style['z-index'] = -1000;
-    //window.document.body.style.background = 'transparent';
-    //const appRoot = window.document.querySelector('app-root') as HTMLElement;
-    //appRoot.style.background = 'transparent';
-
-    //const ionApp = window.document.querySelector('ion-app') as HTMLElement;
-    //ionApp.style.background = 'transparent';
-
+    // Second try, set a low z-index value so the interface goes to the background in ion-app. app-tabs,
+    // ion-tabs, app-tabs3 and ion-content - Doesn't work
+    // Third try - Use the capacitor plugin instead of the cordova one
+    this.toast.show('Soy un toast de ionic', '5000', 'center').subscribe(
+      toast => {
+        console.log(toast);
+      }
+    );
     console.log( 'Llamamos a click' );
+    /*
     this.barcodeScanner
       .scan( optionsBarcodeScanner )
       .then( barcodeData => {
         console.log( 'Data scaned' );
         console.log( 'Barcode data', barcodeData );
+        
+          Object {
+            text: numero del codigo de barras
+            format: tipo codigo barras
+            cancelled: bool
+          }
+        
       } )
       .catch( err => {
         console.log( 'Error' );
         console.log( 'Error', err );
       } );
+      */
   }
 }
